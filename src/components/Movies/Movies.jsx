85749744ffdfa6dbe851df816b2d7459dc14598d
sarrah171996 <sarrahmahmoud714@gmail.com>
2024-04-14 00:39:35 +0200

    u

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,55 +1,53 @@
-// import axios from 'axios';
-import React
-//  { useEffect, useState }
- from 'react'
-// import { Link } from 'react-router-dom';
+import axios from 'axios';
+import React ,{ useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 
 export default function Movies() {
-//   let imgSrc ='https://image.tmdb.org/t/p/original'
-//   const [trendingMovies, setTrendingMovies] = useState([]);
-//   let getTrendingMovies =  async() =>{
-//     let {data} = await axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=e89e2122754e6a1f2189e7163707309d')
-// setTrendingMovies(data.results)
-//     console.log(data.results);
-//   }
+  let imgSrc ='https://image.tmdb.org/t/p/original'
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  let getTrendingMovies =  async() =>{
+    let {data} = await axios.get('https://api.themoviedb.org/3/trending/movie/day?api_key=e89e2122754e6a1f2189e7163707309d')
+setTrendingMovies(data.results)
+    console.log(data.results);
+  }
 
-//   useEffect(() => {
+  useEffect(() => {
     
-//   getTrendingMovies()
+  getTrendingMovies()
    
-//   }, []);
+  }, []);
 
 
   return (
-    <>
-    <h1>movies page</h1>
-    </>
-    // <div>
+    // <>
+    // <h1>movies page</h1>
+    // </>
+    <div>
 
-//             <div className="row gy-5 pt-5">
+            <div className="row gy-5 pt-5">
 
-//               {trendingMovies.map((item,index)=>(
+              {trendingMovies.map((item,index)=>(
 
-//                 <div className="col-md-2" key={index} >
-//                   <Link className='nav-link'  to={`/details/${item.id}/${item.media_type}` }>
-//                   <img className='w-100 mb-3' src={imgSrc + item.poster_path} alt=""  />
-//                   <h2 className='h6'>{item.title}</h2>
-//                   </Link>
+                <div className="col-md-2" key={index} >
+                  <Link className='nav-link'  to={`/details/${item.id}/${item.media_type}` }>
+                  <img className='w-100 mb-3' src={imgSrc + item.poster_path} alt=""  />
+                  <h2 className='h6'>{item.title}</h2>
+                  </Link>
 
-//                 </div>
-
-
-
-//               ))}
+                </div>
 
 
+
+              ))}
 
 
 
 
-//             </div>
 
-    // </div>
+
+            </div>
+
+    </div>
   )
 }
--- src/components/People/People.jsx
+++ src/components/People/People.jsx
@@ -1,23 +1,23 @@
-// import React, { useEffect, useState } from 'react'
-// import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
  
 
 export default function TvShows() {
 
-  // const [tvSowsTrending, setTvSowsTrending] = useState([]);
-  // let imgSrc ='https://image.tmdb.org/t/p/original'
+  const [tvSowsTrending, setTvSowsTrending] = useState([]);
+  let imgSrc ='https://image.tmdb.org/t/p/original'
 
-  // let getTvShowsTrending =  async() => {
-  //   let {data} = await axios.get('https://api.themoviedb.org/3/trending/person/day?api_key=e89e2122754e6a1f2189e7163707309d')
-  //   setTvSowsTrending(data.results)
-  //   console.log(data.results);
+  let getTvShowsTrending =  async() => {
+    let {data} = await axios.get('https://api.themoviedb.org/3/trending/person/day?api_key=e89e2122754e6a1f2189e7163707309d')
+    setTvSowsTrending(data.results)
+    console.log(data.results);
 
-  // }
+  }
 
-  // useEffect(() => {
-  //   getTvShowsTrending()
+  useEffect(() => {
+    getTvShowsTrending()
   
-  // }, []);
+  }, []);
 
 
 
@@ -26,20 +26,20 @@ export default function TvShows() {
 
 
   return (
-    <>
-    <h1>people page</h1>
-    </>
-    // <div>
-    //        <div className="row gy-5 pt-5">
-    //         {tvSowsTrending.map((item,index)=> 
-    //         <div className="col-md-2 " key={index}>
-    //           <img className='w-100 mb-3'  src={imgSrc + item.profile_path} alt="" />
-    //           <h2 className='h6'>{item.name}</h2>
+    // <>
+    // <h1>people page</h1>
+    // </>
+    <div>
+           <div className="row gy-5 pt-5">
+            {tvSowsTrending.map((item,index)=> 
+            <div className="col-md-2 " key={index}>
+              <img className='w-100 mb-3'  src={imgSrc + item.profile_path} alt="" />
+              <h2 className='h6'>{item.name}</h2>
 
-    //         </div>
-    //         )}
-    //        </div>
+            </div>
+            )}
+           </div>
       
-    // </div>
+    </div>
   )
 }
